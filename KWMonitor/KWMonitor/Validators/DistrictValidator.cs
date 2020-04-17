7e804691a2e2a57e6aefdcd1a55346b69ec6dcef
rembies819 <rembies1234@gmail.com>
2020-04-17 12:39:00 -0600

    backend

---
+++ KWMonitor/KWMonitor/Validators/DistrictValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using KoronaWirusMonitor3.Models;
+
+namespace KWMonitor.Validators
+{
+    public class DistrictValidator : AbstractValidator<District>
+    {
+        public DistrictValidator()
+        {
+            RuleFor(district => district.Name)
+                .NotEmpty().WithMessage("To je puste i nie powinno byc")
+                .MinimumLength(3).WithMessage("minimum 3 znaki")
+                .MaximumLength(20).WithMessage("maksymalnie 20 znakow");
+        }
+    }
+}
